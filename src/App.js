f8aea8debd0941e53448cb47cd8a24f063c88211
ssokkara159 <cdn.shiva13@gmail.com>
2024-07-17 10:45:32 +0530

    adding items usngs forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [add, setAdd] = useState("");
+  const [task, setTask] = useState([]);
+
+  const handleOnChange = (e) => {
+    e.preventDefault();
+
+    setAdd(e.target.value);
+  };
+  const handleOnClick = () => {
+    setTask([...task, add]);
+    console.log(task);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>add</div>
+      <select>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+      </select>
+      <form onSubmit={handleOnChange}>
+        <input
+          type="text"
+          placeholder="items..."
+          value={add}
+          onChange={handleOnChange}
+        ></input>
+        <button onClick={handleOnClick}>add</button>
+        <div>
+          list:
+          {task.map((item) => {
+            return <div> {item} </div>;
+          })}
+        </div>
+      </form>
+    </>
   );
-}
+};
 
 export default App;
